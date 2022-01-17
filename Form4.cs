607d7f3336c0371ca7ccc6b0054ec37b4d289e8e
EduCooL <educoolosu@gmail.com>
2022-01-17 14:37:49 +0400

    Коммит 12
    Прописана логика методов и событий четвертой формы

--- Form4.cs
+++ Form4.cs
@@ -27,4 +27,38 @@ namespace Documentation
             Save();
         }
 
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Save();
+        }
+        private void Save()
+        {
+            Excel.Application exApp = new Excel.Application();
+            
+            var book = exApp.Workbooks.Open(path + filename);
+            Excel.Worksheet wsh = (Excel.Worksheet)exApp.Sheets[name];
+            int i, j;
+            for (i = 0; i <= dataGridView1.RowCount - 2; i++)
+            {
+                for (j = 0; j <= dataGridView1.ColumnCount - 1; j++)
+                {
+                    wsh.Cells[i + 2, j + 1] = dataGridView1[j, i].Value.ToString();
+                    wsh.Cells[i + 2, j + 1].EntireColumn.AutoFit();
+                }
+            }
+            book.Save();
+            this.Close();
+        }
+
+        private void Form4_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            Form1 form1 = new Form1();
+            form1.Show();
+        }
+
+        private void toolStripMenuItem2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
 }
