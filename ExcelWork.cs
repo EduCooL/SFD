08a2177cab9571ddb3c7a8113653587faed11811
Datirle <gordeyyurkov@yandex.ru>
2022-01-17 13:57:42 +0400

    Коммит 9
    Прописана сигнатура метода GetData

--- ExcelWork.cs
+++ ExcelWork.cs
@@ -20,39 +20,7 @@ namespace Documentation
         string path = "C:\\Users\\Public\\Documents\\Documentation\\";
         
 
-        public DataTable GetData(string name)
-        {
-          
-            DataTable dt = new DataTable();      
-            
-            Sheet = ObjWorkBook.Sheets[name];
-            var ShtRange = Sheet.UsedRange;
-            for (int Cnum = 1; Cnum <= ShtRange.Columns.Count; Cnum++)
-            {
-                dt.Columns.Add(new DataColumn((ShtRange.Cells[1, Cnum] as Excel.Range).Value2.ToString()));
-            }
-            dt.AcceptChanges();
-            string[] columnNames = new String[dt.Columns.Count];
-            for (int i = 0; i < dt.Columns.Count; i++)
-            {
-                columnNames[0] = dt.Columns[i].ColumnName;
-            }
-
-            for (int Rnum = 2; Rnum <= ShtRange.Rows.Count; Rnum++)
-            {
-                DataRow dr = dt.NewRow();
-                for (int Cnum = 1; Cnum <= ShtRange.Columns.Count; Cnum++)
-                {
-                    if ((ShtRange.Cells[Rnum, Cnum] as Excel.Range).Value2 != null)
-                    {
-                        dr[Cnum - 1] = (ShtRange.Cells[Rnum, Cnum] as Excel.Range).Value2.ToString();
-                    }
-                }
-                dt.Rows.Add(dr);
-                dt.AcceptChanges();
-            }
-            return dt;
-        }
+       
 
         public ExcelWork()
         {
